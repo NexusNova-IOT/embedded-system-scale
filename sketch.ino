56eecdd426021b3acd58df96e09e761021b77387
DominikMendoza <u20201b980@upc.edu.pe>
2023-10-26 10:12:03 -0500

    feat: add weight presentation on display

--- sketch.ino
+++ sketch.ino
@@ -1,10 +1,16 @@
 #include "HX711.h"
 #include <WiFi.h>
+#include <LiquidCrystal_I2C.h>
+#define DELAY 1000
+#define I2C_ADDR    0x27
+#define LCD_COLUMNS 20
+#define LCD_LINES   4
+
+LiquidCrystal_I2C LCD(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-#define DELAY 1000
 HX711 scale;
 
 void setup() {
@@ -22,11 +28,17 @@ void setup() {
   Serial.println("Initializing the scale");
   scale.begin(18, 4);
   scale.set_scale(420.0983);
+  LCD.init();
+  LCD.backlight();
+  LCD.setCursor(0, 0);
 }
 
 void loop() {
   float weight = scale.get_units();
   Serial.print(weight, 1);
   Serial.println(" kg");
+  LCD.clear();
+  LCD.print(weight, 1);
+  LCD.print(" kg");
   delay(DELAY);
 }
